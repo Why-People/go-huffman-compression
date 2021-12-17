d380935de06981f2f8a03e54dc4fadf300258038
Matthew Hayes <matthewha853@gmail.com>
2021-12-17 02:37:32 -0800

    commandline arg parsing

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/akamensky/argparse"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// Argument parsing
+	argparser := argparse.NewParser("huffman", "A simple Huffman Encoder/Decoder written for educational purposes.")
+
+	// File args
+	inFile := argparser.File("i", "inFile", os.O_RDWR, 0600, &argparse.Options{Required: true, Help: "Required Input File"})
+	outFile := argparser.File("o", "outFile", os.O_RDWR, 0600, &argparse.Options{Required: false, Help: "Output File"})
+	
+	// Mode
+	decode := argparser.Flag("d", "decode", &argparse.Options{Required: false, Help: "Set mode to decode"})
+	encode := argparser.Flag("e", "encode",  &argparse.Options{Required: false, Help: "Set mode to encode"})
+	
+	// Parse args
+	err := argparser.Parse(os.Args)
+	if err != nil {
+		fmt.Println(argparser.Usage(err))
+		return
+	}
+
+	// Handle mode args
+	if *decode && *encode {
+		fmt.Println(argparser.Usage("Must specify at most 1 mode flag (-e or -d)"))
+		return
+	}
+
+	if !*decode && !*encode {
+		fmt.Println(argparser.Usage("Must specify at least 1 mode flag (-e or -d)"))
+		return
+	}
+
+	if *encode {
+		fmt.Println("Encode Mode")
+	} else {
+		fmt.Println("Decode Mode")
+	}
+
+	fmt.Println(outFile.Name)
+	fmt.Println(inFile.Name)
 }
