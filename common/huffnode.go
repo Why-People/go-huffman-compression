19a78c9a9bd90db8b60e7c08929761bf58cd2c21
Matthew Hayes <matthewha853@gmail.com>
2021-12-17 16:48:39 -0800

    add huffnode interface

---
+++ common/huffnode.go
@@ -0,0 +1,72 @@
+package common
+
+// Huffman Tree Node
+
+// The public interface
+type HuffNode interface {
+	SetLeft(left *node)
+	SetRight(right *node)
+	Left() *node
+	Right() *node
+	Join(right HuffNode) *node
+}
+
+// Simple Struct to hold the data for a node
+type nodeData struct {
+	symbol byte
+	weight int
+}
+
+// Internal Node Struct
+type node struct {
+	data nodeData
+	left *node
+	right *node
+}
+
+// Sets the left node
+// left: The left node
+func (n *node) SetLeft(left *node) {
+	n.left = left
+}
+
+// Sets the right node
+// right: The right node
+func (n *node) SetRight(right *node) {
+	n.right = right
+}
+
+// Returns the left node
+func (n *node) Left() *node {
+	return n.left
+}
+
+// Returns the right node
+func (n *node) Right() *node {
+	return n.right
+}
+
+// Joins 2 nodes together
+// right: The node to be joined to the right
+func (n *node) Join(right *node) *node {
+	return &node{
+		data: nodeData{
+			symbol: '$',
+			weight: n.data.weight + right.data.weight,
+		},
+		left: n,
+		right: right,
+	}
+}
+
+// Creates a New Node
+// symbol: The symbol to be stored in the node
+// weight: The weight of the node
+func NewNode(symbol byte, weight int) *node {
+	return &node{
+		data: nodeData{
+			symbol: symbol,
+			weight: weight,
+		},
+	}
+}
